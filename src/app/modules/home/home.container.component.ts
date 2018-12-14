f4bcf426ae866f5dfcc1651bffa3e539f3d00a98
Esteban Vera <esteban.vera@rappi.com>
2018-12-13 20:23:47 -0500

    add faker

--- src/app/modules/home/home.container.component.ts
+++ src/app/modules/home/home.container.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as faker from 'faker';
+
+
 @Component({
   selector: 'app-home-container',
   templateUrl: './home.container.component.html',
@@ -11,6 +14,14 @@ export class HomeContainerComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const tomalo = {
+      name: faker.name.findName(),
+      phone: faker.phone.phoneNumberFormat(),
+      image: faker.image.avatar(),
+      id: faker.random.number()
+    };
+
+    console.log(tomalo);
   }
 
 }
